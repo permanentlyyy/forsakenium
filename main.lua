c4cff2296f2d3f2205c55c67a3d544656191173a
permanentlyyy <permanentlyy@proton.me>
2026-08-24 00:02:25 +0200

    Add module loading functionality in main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+print("Forsaken Plus loaded!")
+
+local BASE_URL = "https://raw.githubusercontent.com/permanentlyyy/forsakenium/main/"
+
+local function LoadModule(path)
+    local source = game:HttpGet(BASE_URL .. path)
+    return loadstring(source)()
+end
