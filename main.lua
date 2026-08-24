bc593c86af80a59ff9b5277deff849040065df5d
permanentlyy <permanentlyy@proton.me>
2026-08-24 16:47:46 +0200

    Add Sk8 Control toggle enabling mobile turn control on all skate modes

--- main.lua
+++ main.lua
@@ -141,6 +141,14 @@ VeeronicaSection:AddToggle("AutoTrick", {
     end
 })
 
+VeeronicaSection:AddToggle("Sk8Control", {
+    Title = "Sk8 Control",
+    Default = false,
+    Callback = function(value)
+        Survivors:SetSk8Control(value)
+    end
+})
+
 
 --// Managers
 SaveManager:SetLibrary(Fluent)
